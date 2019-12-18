b1698b8a0363f1e7b4a95857a6da0d5b01db1e69
prajaktagadhave <prajaktagadhave@gmail.com>
2019-12-18 17:44:38 +0530

    WIP on directives

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter  } from '@angular/core';
+import { Ingradient }  from '../../shared/ingradient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+   @ViewChild('nameInput') nameInputRef: ElementRef;
+   @ViewChild('amtInput') amtInputRef: ElementRef;
+   
+   @Output() ingardientAdded = new EventEmitter<Ingradient>();
+       
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    onAddItem(){
+       const inName = this.nameInputRef.nativeElement.value;
+       const inAmt = this.amtInputRef.nativeElement.value; 
+       const newIngradient  = new Ingradient(inName,inAmt);
+       this.ingardientAdded.emit(newIngradient);
+    }
+       
+    
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Ingradient }  from '../shared/ingradient.model';
 
 @Component({
@@ -7,14 +7,26 @@ import { Ingradient }  from '../shared/ingradient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+ @Input() selectedingradName;
  ingradients: Ingradient[] =[
     new Ingradient('apple', 30),
     new Ingradient('banana', 130),
     new Ingradient('eggs', 230),
  ];
+ 
   constructor() { }
 
   ngOnInit() {
   }
+  
+  onIngradientAdded(ingradient: Ingradient){
+    this.ingradients.push(ingradient);
+  }
+  
+  onSelectIngrd(ingradient: Ingradient){
+    this.selectedingradName = ingradient;
+  }
+  
+  
 
 }
